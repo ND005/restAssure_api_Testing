81de79f4973ffa9324692846b96deb0cb5a08f4f
ND005 <nimmaladinesh5@gmail.com>
2025-01-27 09:38:09 +0530

    Update package

---
+++ src/test/java/restAssuredapi/dev/jira_issuecreate_without_discription.java
@@ -0,0 +1,11 @@
+package restAssuredapi.dev;
+
+import org.testng.annotations.Test;
+
+public class jira_issuecreate_without_discription {
+	
+	@Test
+	public void TC01_IssueCreate_withoutDiscription() {
+		
+	}
+}
